d8c28794adee3250b8b6568e47c88093e68e0b71
Arwildo <arwildo@gmail.com>
2019-05-06 13:26:56 +0800

    add gridspec
    plots with different ratio

--- DST_index_subplot.py
+++ DST_index_subplot.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt 
 import matplotlib.dates as mdates
+from matplotlib import gridspec
 import numpy as np 
 import pandas as pd 
 from pandas.plotting import register_matplotlib_converters
@@ -12,21 +13,27 @@ y1 = pd.read_excel('Data/DST.xlsx')
 customdate = datetime.datetime(2015, 3, 1, 0)
 x1 = [customdate + datetime.timedelta(hours=i) for i in range(len(y1))]
 
+#initialize variables Subplot DST
+y2 = pd.read_excel('Data/DST_subplot.xlsx')
+customdate = datetime.datetime(2015, 3, 15, 0)
+x2 = [customdate + datetime.timedelta(hours=i) for i in range(len(y2))]
+
+fig = plt.figure(figsize=(8, 5))
+gs = gridspec.GridSpec(2, 1,
+                   	#	width_ratios=[1, 2],
+                    	height_ratios=[2, 3]
+                       )
+
 #subplot main DST
-plt.subplot(2, 1, 1)
+ax = plt.subplot(gs[0])
 formatter = mdates.DateFormatter('%d')
 plt.gcf().axes[0].xaxis.set_major_formatter(formatter)
 plt.plot(x1, y1, 'r', linewidth=0.75)
 plt.ylabel('DST Index $(nT)$')
 plt.grid(linestyle='dotted')
 
-#initialize variables Subplot DST
-y2 = pd.read_excel('Data/DST_subplot.xlsx')
-customdate = datetime.datetime(2015, 3, 15, 0)
-x2 = [customdate + datetime.timedelta(hours=i) for i in range(len(y2))]
-
 #subplot DST zoomin
-plt.subplot(2, 1, 2)
+ax2 = plt.subplot(gs[1])
 formatter = mdates.DateFormatter('%d')
 plt.gcf().axes[1].xaxis.set_major_formatter(formatter)
 plt.plot(x2, y2, 'r', linewidth=0.75)
