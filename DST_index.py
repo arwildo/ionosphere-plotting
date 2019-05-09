de2b246b19d14200d4d003558c2d8b513a321d36
Arwildo <arwildo@gmail.com>
2019-05-09 22:22:18 +0800

    Minor fix

--- DST_index.py
+++ DST_index.py
@@ -2,9 +2,9 @@ import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 import numpy as np
 import pandas as pd
+import datetime
 from pandas.plotting import register_matplotlib_converters
 register_matplotlib_converters()
-import datetime
 
 customdate = datetime.datetime(2015, 3, 1, 0)
 fig = plt.figure()
@@ -12,7 +12,7 @@ fig.set_figheight(5)
 fig.set_figwidth(10)
 ax = fig.add_subplot(111)
 
-#initialize variables
+# initialize variables
 yo = pd.read_csv('data/DST.csv', delimiter=',', header=None)
 y = yo.values.tolist()
 x = [customdate + datetime.timedelta(hours=i) for i in range(len(y))]
@@ -23,7 +23,7 @@ xmin = x[xpos]
 
 formatter = mdates.DateFormatter('%d')
 
-#annotate
+# annotate
 ax.annotate(
     ymin,
     xy=(xmin, ymin),
@@ -35,7 +35,7 @@ ax.annotate(
                     headlength=8),
 )
 
-#plot
+# plot
 ax.plot(x, y, 'r', linewidth=1)
 plt.gcf().axes[0].xaxis.set_major_formatter(formatter)
 plt.xlabel('March 2015')
--- foF2_subplot.py
+++ foF2_subplot.py
@@ -2,16 +2,16 @@ import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 import numpy as np
 import pandas as pd
+import datetime
 from pandas.plotting import register_matplotlib_converters
 register_matplotlib_converters()
-import datetime
 
-#initialize variables Main DST
+# initialize variables Main DST
 y1 = pd.read_excel('Data/fof2.xlsx')
 customdate = datetime.datetime(2015, 3, 1, 0)
 x1 = [customdate + datetime.timedelta(hours=i) for i in range(len(y1))]
 
-#subplot main DST
+# subplot main DST
 plt.subplot(2, 1, 1)
 formatter = mdates.DateFormatter('%d')
 plt.gcf().axes[0].xaxis.set_major_formatter(formatter)
@@ -19,12 +19,12 @@ plt.plot(x1, y1, 'b', linewidth=0.75)
 plt.ylabel('foF2 $(Mhz)$')
 plt.grid(linestyle='dotted')
 
-#initialize variables Subplot DST
+# initialize variables Subplot DST
 y2 = pd.read_excel('Data/fof2_subplot.xlsx')
 customdate = datetime.datetime(2015, 3, 15, 0)
 x2 = [customdate + datetime.timedelta(hours=i) for i in range(len(y2))]
 
-#subplot DST zoomin
+# subplot DST zoomin
 plt.subplot(2, 1, 2)
 formatter = mdates.DateFormatter('%d')
 plt.gcf().axes[1].xaxis.set_major_formatter(formatter)
