d4fe60a2c2115bb40794dde79afb6788bba200c1
arwildo <arwildo@gmail.com>
2019-07-08 21:38:08 +0800

    Update multiple_yaxis_plot_foF2_DST.py
    change into TEC, foF2, and DST index plot

--- multiple_yaxis_plot_foF2_DST.py
+++ multiple_yaxis_plot_foF2_DST.py
@@ -9,29 +9,39 @@ register_matplotlib_converters()
 # initialize variables
 y1 = pd.read_excel('data/Dst_subplot.xlsx', delimiter=',', header=None)
 y2 = pd.read_excel('data/fof2_subplot.xlsx', delimiter=',', header=None)
-y3 = pd.read_excel('data/fof2_median.xlsx', delimiter=',', header=None)
+y3 = pd.read_excel('data/TEC_subplot.xlsx', delimiter=',', header=None)
 customdate = datetime.datetime(2015, 3, 15, 0)  # date start
 x = [customdate + datetime.timedelta(hours=i) for i in range(len(y1))]
 
 # plot config
-fig, host = plt.subplots(figsize=(10, 5))
+fig, host = plt.subplots(figsize=(14, 7))
 plt.grid(linestyle='-')
 par1 = host.twinx()
+par2 = host.twinx()
 
 p1, = host.plot(x, y1, "r-", label="DST")
 p2, = par1.plot(x, y2, "b-", label="foF2")
-p3, = par1.plot(x, y3, "lime", label="Median foF2")
+p3, = par2.plot(x, y3, "black", label="TEC")
+
+# add minor grid
+host.minorticks_on()
+par1.minorticks_on()
+
+par2.spines['right'].set_position(('outward', 60))
 
 host.set_xlabel("Maret 2015")
 host.set_ylabel("Indeks DST $(nT)$")
 par1.set_ylabel("foF2 $(Mhz)$")
+par2.set_ylabel('TEC $(TECU)$')
 
 host.yaxis.label.set_color(p1.get_color())
 par1.yaxis.label.set_color(p2.get_color())
+par2.yaxis.label.set_color(p3.get_color())
 
 tkw = dict(size=4, width=1.5)
 host.tick_params(axis='y', colors=p1.get_color(), **tkw)
 par1.tick_params(axis='y', colors=p2.get_color(), **tkw)
+par2.tick_params(axis='y', colors=p3.get_color(), **tkw)
 host.tick_params(axis='x', **tkw)
 
 lines = [p1, p2, p3]
@@ -39,5 +49,8 @@ lines = [p1, p2, p3]
 host.legend(lines, [l.get_label() for l in lines], loc='lower right')
 formatter = mdates.DateFormatter('%d')
 plt.gcf().axes[0].xaxis.set_major_formatter(formatter)
+
+# minor grid activated
+host.grid(which='minor', linestyle=':', linewidth='0.5', color='black')
 
 plt.show()
